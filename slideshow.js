25a90c70d8d242d6a74f3f570a8280bf0d31637b
sam mackay <sammackay32@gmail.com>
2016-06-09 20:00:17 +1200

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,40 @@
+ $(document).ready(function() {
+     $('.slideshow').slick({
+       infinite: true,
+       speed: 1000,
+       slidesToShow: 1,
+       adaptiveHeight: true,
+       autoplay: true,
+       autoplaySpeed: 2000,
+       infinite: false,
+       dots: false,
+
+	
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ]
+});
+});				
